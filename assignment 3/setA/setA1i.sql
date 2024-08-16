74f1f6949c6b4817aa8fff3778f1c9ad327f4108
VaibhavFanClub <vaibhavfanclub2@gmail.com>
2024-08-16 09:35:17 +0530

    Delete assignment 3 directory

--- assignment 3/setA/setA1i.sql
+++
@@ -1,8 +0,0 @@
-create or replace function count_emp_2007() returns integer as $$
-Declare
-	emp_count integer;
-Begin
-	select count(*) into emp_count from employee where joining_date < '01-01-2007';
-	return emp_count;
-End
-$$ language 'plpgsql';
--- assignment 3/setA/setA2i.sql
+++
@@ -1,8 +0,0 @@
-create or replace function A3A2F1(name varchar) returns integer as $$
-Declare
-	count_p integer;
-Begin
-	select count(pno) into count_p from person where aname = name;
-	return  count_p;
-End;
-$$ language 'plpgsql';
